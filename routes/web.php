d1a690ac1ced98d2c52ed5e9d804d454212ee3ec
Nguyen Hoang <quynh4589@gmail.com>
2018-05-15 20:42:41 +0700

    Add to cart

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,14 @@ Route::group(['middleware'=> 'auth', 'prefix' => 'user', 'namespace' => 'User\Pr
     Route::get('changePassword', 'ProfileController@indexChangePassword')->name('changePass');
     Route::post('changePassword', 'ProfileController@storeChangePassword')->name('storePass');
 });
+
+//Cart
+Route::namespace('User')->group(function () {
+    Route::get('cart', 'CartController@index')->name('cart');
+    Route::post('cart', 'CartController@store')->name('storeCart');
+    Route::get('product', 'CartController@show')->name('showProduct');
+    Route::post('increment/{id}', 'CartController@incrementQty')->name('incrementQty');
+    Route::post('decrement/{id}', 'CartController@decrementQty')->name('decrementQty');
+    Route::post('destroyItem', 'CartController@destroy')->name('destroyItemCart');
+    Route::get('destroyCart', 'CartController@destroyCart')->name('destroyCart');
+});
